52c5711289843e0c179bc8298e03e5f6b3996437
Jungian1972 <142367330+Jungian1972@users.noreply.github.com>
2024-06-20 22:15:51 -0400

    Update Confidence and Credibility Intervals for League Champs.R

--- Confidence and Credibility Intervals for League Champs.R
+++ Confidence and Credibility Intervals for League Champs.R
@@ -1,6 +1,7 @@
 ##Produces a win/lose data set of size N
 N = 4000
-Simulated_Data <- rbinom(size = 1, n = N, prob = .5)
+Win_Rate = .52
+Simulated_Data <- rbinom(size = 1, n = N, prob = Win_Rate)
 
 ##Calculates the confidence interval.. typical frequentist approach
 Confidence_Interval <- qbinom(p = c(0.025, 0.975), size = length(Simulated_Data), prob = mean(Simulated_Data))/length(Simulated_Data)
@@ -8,7 +9,7 @@ Confidence_Interval
 
 ##Calculates the Bayesian Credibility Interval which is similar to the confidence interval, but has an easier interpretation
 Credibility_Interval <- c() %>%
-  append(qbeta(0.025, (length(Simulated_Data)-sum(Simulated_Data == 1)+1), (sum(Simulated_Data == 1)+1))) %>%
-  append(qbeta(0.975, (length(Simulated_Data)-sum(Simulated_Data == 1)+1), (sum(Simulated_Data == 1)+1)))
+  append(qbeta(0.025, (sum(Simulated_Data == 1)+1), (length(Simulated_Data)-sum(Simulated_Data == 1)+1))) %>%
+  append(qbeta(0.975, (sum(Simulated_Data == 1)+1), (length(Simulated_Data)-sum(Simulated_Data == 1)+1)))
 Credibility_Interval  
          
